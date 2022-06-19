540ebbe4a7bada0c16f441da746e5e9e2c376201
Finnegan Beaudin <noah.beaudin1@gmail.com>
2022-06-19 15:23:23 -0600

    Updated album preview to contain album data

--- src/pages/AlbumPreview.svelte
+++ src/pages/AlbumPreview.svelte
@@ -1,12 +1,24 @@
-<script>
-import { showDarkMode } from "../data/controller";
+<script lang="ts">
+import { onMount } from "svelte";
+import SongItem from "../components/song/SongItem.svelte";
+
+import { albumToPreview, showDarkMode } from "../data/controller";
+import { music } from "../data/data";
+
+const album = $albumToPreview.album
+const artist = $albumToPreview.artist
+let songsInAlbum: MusicObject[] = []
+
+onMount(() => {
+    songsInAlbum = $music.filter(item => item.album === album) 
+})
 </script>
 
 <div class="album-preview {$showDarkMode ? "dark-mode" : ""}">
     <header>
         <div class="text">
-            <h1>[Album]</h1>
-            <h2>[Artist]</h2>
+            <h1>{album}</h1>
+            <h2>{artist}</h2>
         </div>
         <div class="buttons">
             <button>[Play]</button>
@@ -14,7 +26,9 @@ import { showDarkMode } from "../data/controller";
     </header>
 
     <div class="songs">
-        <p>[Songs placeholder]</p>
+        {#each songsInAlbum as song}
+            <SongItem {...song}/>
+        {/each}
     </div>
 </div>
 
@@ -22,9 +36,12 @@ import { showDarkMode } from "../data/controller";
     @import "../styles/variables.scss";
     @import "../styles/components/index";
 
+    // FIXME: Not being able to scroll down to final item on overflow
+
     .album-preview {
         padding-left: 20px;
         padding-right: 20px;
+        height: fit-content;
 
         header {
             display: flex;
@@ -32,6 +49,16 @@ import { showDarkMode } from "../data/controller";
             .buttons {
                 margin-left: auto;
             }
+        }
+
+        .songs {
+            display: flex;
+            flex-direction: column;
+
+            justify-content: center;
+
+            padding-left: 10%;
+            padding-right: 10%;
         }
     }
 </style>
